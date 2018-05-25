e907591c3fd390150819c992c8c1ff66d46c0903
Alex Bliss <alexander.blss@gmail.com>
2018-05-24 23:58:26 -0500

    adjusting result location

--- src/App.js
+++ src/App.js
@@ -8,9 +8,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Whats My Age Again</h1>
+          <h1 className="App-title">Whats My Age Again?</h1>
         </header>
         <p className="App-intro">
+        <h2>enter birthdate</h2>
+        <input placeholder="Birth Date"></input>
+        <button>Submit</button>
+        <h3>result</h3>
         </p>
       </div>
     );
